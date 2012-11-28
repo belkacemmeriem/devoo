9e87d31702b7760df3507892a5d6ffa44d2b0825
Garcia Andréas <andreas.garcia@insa-lyon.fr>
2012-11-28 08:32:43 +0100

    Premiere fenetre

---
+++ Supervision/src/supervision/Supervision.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supervision;
+
+/**
+ *
+ * @author Mignot
+ */
+public class Supervision extends javax.swing.JPanel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Fenetre fenetre = new Fenetre();
+        fenetre.setVisible(true);
+    }
+}
