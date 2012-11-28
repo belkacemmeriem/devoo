3de5bd3e852dbd8f70f5a5b8c15355038fcc97b6
Anis <Benyoub.anis@gmail.com>
2012-11-28 10:54:25 +0100

    Adding limited search dijkstra

--- model_prj/src/dijkstra/Dijkstra.java
+++ model_prj/src/dijkstra/Dijkstra.java
@@ -39,37 +39,8 @@ public class Dijkstra {
         }
         return node;
     }
-    public   List<Node> Dijkstra(ZoneGeo theGraph, Node source )
- /*
- 1  function Dijkstra(Graph, source):
- 2      for each vertex v in Graph:                                // Initializations
- 3          dist[v] := infinity ;                                  // Unknown distance function from 
- 4                                                                 // source to v
- 5          previous[v] := undefined ;                             // Previous node in optimal path
- 6      end for                                                    // from source
- 7      
- 8      dist[source] := 0 ;                                        // Distance from source to source
- 9      Q := the set of all nodes in Graph ;                       // All nodes in the graph are
-10                                                                 // unoptimized - thus are in Q
-11      while Q is not empty:                                      // The main loop
-12          u := vertex in Q with smallest distance in dist[] ;    // Start node in first case
-13          remove u from Q ;
-14          if dist[u] = infinity:
-15              break ;                                            // all remaining vertices are
-16          end if                                                 // inaccessible from source
-17          
-18          for each neighbor v of u:                              // where v has not yet been 
-19                                                                 // removed from Q.
-20              alt := dist[u] + dist_between(u, v) ;
-21              if alt < dist[v]:                                  // Relax (u,v,a)
-22                  dist[v] := alt ;
-23                  previous[v] := u ;
-24                  decrease-key v in Q;                           // Reorder v in the Queue
-25              end if
-26          end for
-27      end while
-28  return dist;
-*/      
+    public   List<Node> Dijkstra(ZoneGeo theGraph, Node source, List<Node> toEnglobe)
+
     {
         HashMap<Integer,Integer> dist = new HashMap<Integer, Integer>(); 
         HashMap<Integer,Integer> previous = new HashMap<Integer, Integer>(); 
@@ -89,8 +60,9 @@ public class Dijkstra {
         }
         PriorityNode aNode =  new PriorityNode(0,source.getID());
         Q.add(aNode);
-        while(Q.size()!=0)
+        while((Q.size()!=0)&& (toEnglobe.size()!=0))
         {
+        	
             Node u = theGraph.getNode(getMinVertex(dist));
             Q.remove(u.getID());
             if(dist.get(u.getID())==Infinity)
@@ -110,7 +82,13 @@ public class Dijkstra {
                     }
                 }
             }
-            
+            for(Node v : toEnglobe)
+            {
+            	if(v.getID()==u.getID())
+            	{
+            		toEnglobe.remove(v);
+            	}
+            }
         }
         List<Node> Solution= new ArrayList<Node>();
         return Solution;
