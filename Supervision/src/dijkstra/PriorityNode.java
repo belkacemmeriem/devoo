5d6f492f6784acb8208839be9059eddcf8b5bd59
Fred <fred.matigot@gmail.com>
2012-12-01 12:25:41 +0100

    poursuite integration

---
+++ Supervision/src/dijkstra/PriorityNode.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dijkstra;
+
+/**
+ *
+ * @author anisbenyoub
+ */
+public class PriorityNode {
+    protected
+            Integer id;
+            Integer distance;
+            public PriorityNode(Integer aDistance,Integer anID)
+            {
+                id=anID;
+                distance= aDistance;
+            }
+    
+}
