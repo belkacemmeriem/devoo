ae55e7de937475d72beb7c71e9356e87cb9118ea
Fred <fred.matigot@gmail.com>
2012-12-03 09:36:39 +0100

    debut view feuille de route

---
+++ Supervision/src/views/ViewSchedule.java
@@ -0,0 +1,19 @@
+package views;
+
+import java.awt.Graphics;
+
+import model.Schedule;
+
+public class ViewSchedule {
+	Schedule schedule;
+	ViewMain mere;
+
+	public ViewSchedule(Schedule s, ViewMain m) {
+		schedule = s;
+		mere = m;
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+}
