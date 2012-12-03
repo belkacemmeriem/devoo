9a82fed562ed9979af7eea44293f722c63dec09d
Fred <fred.matigot@gmail.com>
2012-12-03 10:48:22 +0100

    merge ok

---
+++ Supervision/src/views/ViewDelivery.java
@@ -0,0 +1,20 @@
+package views;
+
+import java.awt.Graphics;
+
+import model.Delivery;
+
+public class ViewDelivery {
+	
+	Delivery delivery;
+	ViewMain mere;
+
+	public ViewDelivery(Delivery d, ViewMain m) {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+
+}
