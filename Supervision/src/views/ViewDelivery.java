7a0c44baacd98481871ba6dcd779e099adca33d1
Fred <fred.matigot@gmail.com>
2012-12-05 09:20:46 +0100

    vue arcs de delivery

--- Supervision/src/views/ViewDelivery.java
+++ Supervision/src/views/ViewDelivery.java
@@ -1,7 +1,9 @@
 package views;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
+import model.Arc;
 import model.Delivery;
 import model.Node;
 
@@ -10,15 +12,24 @@ public class ViewDelivery {
 	Delivery delivery;
 	ViewMain mere;
 	ViewNode dest;
+	ArrayList<ViewArc> arcs = new ArrayList<ViewArc>();
 
 	public ViewDelivery(Delivery d, ViewMain m) {
 		delivery = d;
 		mere = m;
+		
 		Node destNode = delivery.getDest();
 		dest = new ViewNode(destNode, mere);
 		dest.setMyDefaultColor(delivery.getSchedule().getColor());
 		dest.setMyDefaultRadius(8);
 		dest.setDefault();
+		
+		for (Arc a : delivery.getPathToDest().getArcs()) {
+			ViewArc va = new ViewArc(a, mere);
+			va.setColor(delivery.getSchedule().getColor());
+			va.setEpaisseur(3);
+			arcs.add(va);
+		}
 	}
 	
 	public void paint(Graphics g, boolean onlyDest) {
